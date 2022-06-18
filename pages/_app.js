a931eaf9b2333c4d49a91bde57212ae34348ab6b
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-17 22:36:50 -0400

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+//import from node_modules "ant design"
+// path =  "../../"
+import "antd/dist/antd.css";
+//import app from 'next/app'
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+  
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+
+//using state to store values
+function Home () {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button type="primary">Click me</Button>
+        </div>
+    );
+}
+
+export default Home;
